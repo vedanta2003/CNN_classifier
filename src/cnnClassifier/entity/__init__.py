a99b5809719b7a17d68ba07ebb8584fb502a1724
vedanta2003 <108720289+vedanta2003@users.noreply.github.com>
2024-11-18 11:27:38 +0530

    first commit

---
+++ src/cnnClassifier/entity/__init__.py
@@ -0,0 +1,5 @@
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig,
+                                                EvaluationConfig)
